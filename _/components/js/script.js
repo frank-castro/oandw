dca7ec8275055e64dce75c2a595934c45e04762f
frank-castro <contact@frankcastro.com>
2015-08-28 01:39:41 -0400

    about section with functionality added

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,17 +1,14 @@
 $('.barsWrapper').on('click', function(){
-  $('.sitemenu, .navwrapper a').toggleClass('hidden');
-  
-  return false;
+	$('.sitemenu, .navwrapper a').toggleClass('hidden');
+	return false;
 });
 
 $('.navwrapper a').on('click', function(){
-  $('.sitemenu').toggleClass('hidden');
-  
-  return true;
+	$('.sitemenu').toggleClass('hidden');
+	return true;
 });
 
-$('.infoIcon').on('click', function(){
-  $('.popup').toggleClass('hidden');
-  
-  return false;
+$('.infoIcon, .about').on('click', function(){
+	$('.popup').toggleClass('hidden');
+	$('.sitemenu').toggleClass('movedown');
 });
