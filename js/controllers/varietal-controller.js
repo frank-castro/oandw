c1d9e703d9a3d7d67c54c2921914dab7a334a53e
frank-castro <contact@frankcastro.com>
2015-10-08 00:58:46 -0400

    intro page working with click and scroll, missing refresh in other sections

--- js/controllers/varietal-controller.js
+++ js/controllers/varietal-controller.js
@@ -2,14 +2,14 @@ angular.module('storeProducts')
 
 	// $scope and $http after bracket protects this function from issues with minification, the bracket close at the bottom
 
-.controller('varietalList', ['$scope', '$http', 'orderService', function($scope, $http, orderService) {
+.controller('varietalList', ['$scope', '$http', '$timeout', 'orderService', function ($scope, $http, $timeout, orderService) {
 	var list = this;
 	list.product = []; //empty array so you don't get an error when the page initially loads
 	$http.get('json/varietal.json').success(function(data) {
 		$scope.product = data;
 	});
 
-//cart functionality
+   //cart functionality
 	$scope.add = function (prod, vol, price) {
 	    orderService.add(prod, vol, price);
 	}
@@ -30,4 +30,30 @@ angular.module('storeProducts')
 	    return orderService.submit();
 	}
 
+	$scope.splashOn = orderService.splashOn;
+
+	$scope.headerClass = function () {
+	    if (orderService.splashOn) {
+	        return 'header headerIntro';
+	    } else {
+	        return 'header';
+	    }
+	}
+
+	$scope.closeSplash = function () {
+	    orderService.splashOn = false;
+	    $scope.splashOn = false;
+	    $('.sitemenu').removeClass('sitemenuIntro');
+	    $('.imgText').addClass('imgTextIntro');
+	    $('.logo').removeClass('logoIntro');
+	}
+
+	$(window).scroll(function () {
+	    if (orderService.splashOn) {
+	        var topOfWindow = $(this).scrollTop();
+	        if (topOfWindow > 70) {
+	            $timeout(function () { $scope.closeSplash(); }, 100);
+	        }
+	    }
+	});
 }]);
