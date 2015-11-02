23dfddb0772e651df7016615f03093f15f65ed9a
frank-castro <contact@frankcastro.com>
2015-11-01 23:40:14 -0500

    new files added

---
+++ js/directives/seasoning-container.js
@@ -0,0 +1,18 @@
+angular.module('storeProducts')
+
+.directive("seasoningContainer", function() {
+	return {
+		template: '<ng-include src="getTemplateUrl()"/>',
+		restrict: "E",
+		controller: function($scope) {
+			$scope.getTemplateUrl = function() {
+				if ($scope.item.type == "package1")
+					return "templates/directives/package1.html";
+				if ($scope.item.type == "package2")
+					return "templates/directives/package2.html";
+				if ($scope.item.type == "jar")
+					return "templates/directives/jar.html";
+			}
+		}
+	};
+});
---
+++ js/directives/specialty-container.js
@@ -0,0 +1,16 @@
+angular.module('storeProducts')
+
+.directive("specialtyContainer", function() {
+	return {
+		template: '<ng-include src="getTemplateUrl()"/>',
+		restrict: "E",
+		controller: function($scope) {
+			$scope.getTemplateUrl = function() {
+				if ($scope.item.type == "normalbottle")
+					return "templates/directives/normal-bottle.html";
+				if ($scope.item.type == "specialbottle")
+					return "templates/directives/special-bottle.html";
+			}
+		}
+	};
+});
