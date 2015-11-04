13f9a3fa343e675fd6e251cc9364a15458823b63
frank-castro <contact@frankcastro.com>
2015-11-04 00:09:57 -0500

    mobile menu added, about us changed and other stuff

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -14,18 +14,20 @@ $('.closeBttn').on('click', function(){
 	$('.aboutUs').addClass('closed');
 });
 
+$('.closeBttnMenu').on('click', function(){
+	$('.sitemenu').toggleClass('hidden visible');
+});
+
 $('.shopping_cart, #keepShopping').on('click', function(){
 	$('.cartBanner').toggleClass('cartBttn');
 	$('.cartBannerContent').toggleClass('hidden');
 });
 
+// ** old stuff you had here **
+
 $(window).scroll(function(){
   var windowHeight = $(this).height();
   var topOfWindow = $(this).scrollTop();
-
-	// if (topOfWindow > 300) {
-	// 	$('.aboutUs').addClass('closed');
-	// }
   
   $('.submenu').each(function() {
     var elemPosit = $(this).offset().top;
@@ -35,6 +37,52 @@ $(window).scroll(function(){
     } else {
       $(this).addClass("hidden");
     }
-    
+	
   });
 });
+
+
+
+// ** new stuff starts here **
+
+// variables to hide nav menu on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.logoBar').outerHeight();
+
+$(window).on('scroll', function() {
+
+	didScroll = true;
+});
+
+// functionality to hide nav menu on scroll
+setInterval(function() {
+	if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+	var st = $(this).scrollTop();
+  
+	// Make sure they scroll more than delta
+	if(Math.abs(lastScrollTop - st) <= delta)
+		return;
+  
+	// If they scrolled down and are past the navbar, add class .nav-up.
+	// This is necessary so you never see what is "behind" the navbar.
+	if (st > lastScrollTop && st > navbarHeight) {
+	// Scroll Down
+		$('.logoBar').removeClass('show').addClass('hide');
+	} else {
+	// Scroll Up
+		if(st + $(window).height() < $(document).height()) {
+			$('.logoBar').removeClass('hide').addClass('show');
+		}
+	}
+
+	lastScrollTop = st;
+
+}
--- js/controllers/order-controller.js
+++ js/controllers/order-controller.js
@@ -9,6 +9,11 @@ var Controllers;
             this.orderService = orderService;
             // wizard step
             this.step = 1;
+            this.shippingOptions = [
+                { value: 0, name: 'Standard Shipping' },
+                { value: 1, name: '3rd Business Day (Additional $30)' },
+                { value: 2, name: '2nd Business Day Air (Additional $40)' }
+            ];
             //#region Cart
             this.recentlyChanged = false;
             this.maximized = false;
@@ -18,6 +23,17 @@ var Controllers;
             // order accessor
             get: function () {
                 return this.orderService.order;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(OrderController.prototype, "shippingOption", {
+            get: function () {
+                var idx = this.order.shippingType || 0;
+                return this.shippingOptions[idx];
+            },
+            set: function (option) {
+                this.order.shippingType = option.value || 0;
             },
             enumerable: true,
             configurable: true
@@ -94,4 +110,3 @@ var Controllers;
     angular.module('storeProducts').controller('orderController', ['$scope', '$timeout', 'orderService',
         function ($scope, $timeout, orderService) { return new OrderController($scope, $timeout, orderService); }]);
 })(Controllers || (Controllers = {}));
-//# sourceMappingURL=order-controller.js.map
